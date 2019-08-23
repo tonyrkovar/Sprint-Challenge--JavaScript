c65db6e06571c38e16333e6283e4b67753a501f6
Tony <tonyrkovar@gmail.com>
2019-08-23 12:02:28 -0500

    finished prototypes

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -93,10 +93,10 @@ console.log(contactInfo);
 /* Request 3: Find out how many universities have the string "Uni" included in their name.
  Create a new array called unisWithUni that 
 contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = [];
-for(uniName of graduates){
-  if(uniName === "Salem University"){unisWithUni.push(uniName)}
-}
+// const unisWithUni = [];
+// for(uniName of graduates){
+//   if(uniName === "Salem University"){unisWithUni.push(uniName)}
+// }
 
 
 
@@ -109,7 +109,7 @@ for(uniName of graduates){
 //   })
 // }
 
-console.log(unisWithUni);
+// console.log(unisWithUni);
 
 
 // ==== ADVANCED Array Methods ====
